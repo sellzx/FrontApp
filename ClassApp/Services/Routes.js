105277382eb08c0de758cfa2449f0ebcdd64d8bc
sellzx <simlara89@gmail.com>
2023-04-05 00:09:55 -0400

    se maneja la ruta para apuntarla a equipo EC2 en AWS

--- ClassApp/Services/Routes.js
+++ ClassApp/Services/Routes.js
@@ -1,4 +1,4 @@
- const ApiRoute = "https://localhost:44361/api/";
+//  const ApiRoute = "https://localhost:44361/api/";
 //poner esta ruta a elasticbeanstalk para mostrar la tarea
-//const ApiRoute = "http://backapp-prod.us-east-1.elasticbeanstalk.com/api/";  
+const ApiRoute = "http://backapp-prod.us-east-1.elasticbeanstalk.com/api/";  
   export default ApiRoute;
